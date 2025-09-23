edfd45a08f50f06cedb11330d2727f53305ad555
SoraEien <dmitriygolitsyn03@gmail.com>
2025-09-23 23:28:45 +0300

    V 0.2

---
+++ HardwareMonitoring/HardwareInfo/HardwareManager.cs
@@ -0,0 +1,49 @@
+﻿using HardwareTemperature.HardwareInfo;
+using LibreHardwareMonitor.Hardware;
+
+namespace HardwareMonitoringClient.HardwareInfo
+{
+    public class HardwareManager
+    {
+        private Computer _computer;
+
+        public HardwareManager()
+        {
+            _computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMotherboardEnabled = true,
+            };
+            _computer.Open();
+        }
+
+        public void Close() => _computer.Close();
+
+        public async Task Monitor()
+        {
+            _computer.Accept(new UpdateVisitor());
+
+            foreach (IHardware hardware in _computer.Hardware)
+            {
+                Console.WriteLine("{0}", hardware.Name);
+
+                if (hardware.HardwareType == HardwareType.Motherboard)
+                {
+                    foreach (IHardware subhardware in hardware.SubHardware)
+                    {
+                        foreach (ISensor sensor in subhardware.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Temperature)
+                                Console.WriteLine("\t{0}, temperature: {1} C", sensor.Name, sensor.Value);
+                        }
+                    }
+                }
+
+                foreach (ISensor sensor in hardware.Sensors)
+                    if (sensor.Name.Contains("GPU") || sensor.Name.Contains("CPU") || sensor.SensorType == SensorType.Temperature)
+                        Console.WriteLine("\t{0}, {2}: {1}", sensor.Name, sensor.Value, sensor.SensorType);
+            }
+        }
+    }
+}
---
+++ HardwareMonitoring/Program.cs
@@ -0,0 +1,68 @@
+﻿using HardwareMonitoringClient.HardwareInfo;
+using HardwareTemperature.HardwareInfo;
+using Microsoft.Extensions.Configuration;
+
+namespace HardwareMonitoringClient
+{
+    class Program
+    {
+        private static Timer timer;
+        private static HardwareDisplay temperatureDisplay;
+
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            long interval = long.Parse(configuration["TimerSettings:Interval"]);
+
+            HardwareManager hardwareManager = new HardwareManager();
+            temperatureDisplay = new HardwareDisplay(hardwareManager);
+
+            // Инициализация таймера
+            timer = new Timer(TimerCallback, null, 0, interval);
+
+            // Ждем нажатия Enter для выхода
+            Console.ReadLine();
+
+            // Остановка таймера и освобождение ресурсов
+            StopTimer();
+            hardwareManager.Close();
+        }
+
+        private static void TimerCallback(object state)
+        {
+            // Вызываем метод для отображения значений датчиков
+            temperatureDisplay.DisplaySensorsValues();
+        }
+
+        private static void StopTimer()
+        {
+            timer?.Change(Timeout.Infinite, Timeout.Infinite);
+            timer?.Dispose();
+        }
+        //static void Main(string[] args)
+        //{
+        //    var configuration = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("appsettings.json")
+        //        .Build();
+
+        //    long interval = long.Parse(configuration["TimerSettings:Interval"]);
+
+        //    HardwareManager hardwareManager = new HardwareManager();
+        //    HardwareDisplay temperatureDisplay = new HardwareDisplay(hardwareManager);
+
+        //    Timer timer = new Timer();
+        //    timer.Enabled = true;
+        //    timer.Interval = interval;
+        //    timer.Elapsed += (sender, e) => temperatureDisplay.DisplaySensorsValues();
+
+        //    // Ждем нажатия Enter для выхода
+        //    Console.ReadLine();
+        //    hardwareManager.Close();
+        //}
+    }
+}
